e844c2bb28794208fa9624ceef2d1558ce520b3a
mlee041317 <michelleleefit@gmail.com>
2021-06-05 22:11:17 -0700

    reworked html, and css

--- app/app.js
+++ app/app.js
@@ -1,1 +1,9 @@
+// const formInput = document.getElementById('name', 'email', 'subject');
+// document.querySelector('.form').addEventListener('submit', function (e) {
+//     e.preventDefault();
+//     alert(`Thank you!`);
+// })
+
+
+
 
